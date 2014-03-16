1cc67aa57ca361e2709f80e8d024b602ec8bd536
cmgdev <cmgdev>
2014-03-16 09:33:15 -0500

    in progress -- moving the http get to a function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+
 var app = express();
 
 app.get('/wotd/:year/:month', function(req, res){
@@ -7,20 +8,27 @@ app.get('/wotd/:year/:month', function(req, res){
 		+ req.params.year + "/" 
 		+ req.params.month + "/";
 	
-	data = "";
-	http.get(wotdPath, function(getRes) {
-		console.log("Got response: " + getRes.statusCode);
-		getRes.on('data', function(chunk){
-			data += chunk;
-		});
-		getRes.on('end', function() {
-			res.send(data);
-		});
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
-	});
+	doHttpGet(wotdPath, res);
 });
 
 var server = app.listen(3000, function(){
 	console.log('Listening on port %d', server.address().port);
 });
+
+var doHttpGet = function(path, res){
+	http.get(wotdPath, function(getRes) {
+		console.log("Got response: " + getRes.statusCode);
+		data = "";
+		getRes.on('data', function(chunk){
+			data += chunk;
+		});
+		getRes.on('end', function() {
+			console.log(data);
+			res.send(data);
+		});
+	}).on('error', function(e) {
+		errorMsg = "Got error: " + e.message;
+		console.log(errorMsg);
+		res.send(errorMsg);
+	});
+};
