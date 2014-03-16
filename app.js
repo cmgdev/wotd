91801418c7945b916a5f4c5e4f6a367b744c269a
cmgdev <cmgdev>
2014-03-16 10:10:05 -0500

    added date validation with moment.js

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var http = require('http');
+var moment = require('moment');
 
 var app = express();
 
 app.get('/wotd/:year/:month', function(req, res){
-	wotdPath = "http://dictionary.reference.com/wordoftheday/archive/" 
-		+ req.params.year + "/" 
-		+ req.params.month + "/";
+	year = req.params.year;
+	month = req.params.month;
+	// the first day of last month
+	lastCompleteMonth = moment().subtract('months', 1).date(1);
+	requestedMonth = moment([year, month - 1]);
 	
-	doHttpGet(wotdPath, res);
+	if(requestedMonth.isBefore(lastCompleteMonth)){
+		wotdPath = "http://dictionary.reference.com/wordoftheday/archive/" 
+			+ req.params.year + "/" 
+			+ req.params.month + "/";
+	
+		doHttpGet(wotdPath, res);
+	}
+	else{
+		message = "You requested the words for " + requestedMonth.format('YYYY/MM') 
+			+ ", but the latest date you can request is " + lastCompleteMonth.format('YYYY/MM')
+			+ ". Please pick another date or try again on the first of next month.";
+		res.send(message);
+	}
 });
 
 var server = app.listen(3000, function(){
@@ -23,7 +38,7 @@ var doHttpGet = function(path, res){
 			data += chunk;
 		});
 		getRes.on('end', function() {
-			console.log(data);
+			//console.log(data);
 			res.send(data);
 		});
 	}).on('error', function(e) {
